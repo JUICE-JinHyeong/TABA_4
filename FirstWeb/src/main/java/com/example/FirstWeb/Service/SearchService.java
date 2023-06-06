9849a3b0430857c12721c2bcd795855bfda6bf54
E0min <59902527+E0min@users.noreply.github.com>
2023-06-06 14:50:27 +0900

    cm

--- FirstWeb/src/main/java/com/example/FirstWeb/Service/SearchService.java
+++ FirstWeb/src/main/java/com/example/FirstWeb/Service/SearchService.java
@@ -5,17 +5,14 @@ import com.example.FirstWeb.repository.SearchInputCountRepository;
 import com.example.FirstWeb.dto.SearchInputData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
-
+import javax.transaction.Transactional;
 import java.util.Date;
+
 @Service
 public class SearchService {
-    @PersistenceContext
-    private EntityManager entityManager;
+
     private final SearchInputCountRepository searchInputCountRepository;
     private final RestTemplate restTemplate;
 
@@ -23,6 +20,26 @@ public class SearchService {
     public SearchService(SearchInputCountRepository searchInputCountRepository, RestTemplate restTemplate) {
         this.searchInputCountRepository = searchInputCountRepository;
         this.restTemplate = restTemplate;
+    }
+    public void saveSearch(String searchInput) {
+        SearchInputCount searchInputCount = searchInputCountRepository.findBySearchInput(searchInput);
+
+        if (searchInputCount != null) {
+            // If the search input exists, increase the count
+            searchInputCount.setCount(searchInputCount.getCount() + 1);
+        } else {
+            // If the search input does not exist, create a new record
+            searchInputCount = SearchInputCount.builder()
+                    .searchInput(searchInput)
+                    .count(1)
+                    .date(new Date())
+                    .build();
+        }
+
+        searchInputCountRepository.save(searchInputCount); // 저장된 엔티티 수정 후 다시 저장
+
+        // Send data to server using restTemplate
+        sendSearchDataToServer(searchInput);
     }
 
     private void sendSearchDataToServer(String searchInput) {
@@ -32,33 +49,5 @@ public class SearchService {
 
         // Send POST request to server
         restTemplate.postForObject(url, requestData, Void.class);
-    }
-
-    @Transactional
-    public void saveSearch(String searchInput) {
-
-            SearchInputCount searchInputCount = searchInputCountRepository.findBySearchInput(searchInput);
-
-            if (searchInputCount != null) {
-                // If the search input exists, increase the count
-                searchInputCount.setCount(searchInputCount.getCount() + 1);
-            } else {
-                // If the search input does not exist, create a new record
-                searchInputCount = SearchInputCount.builder()
-                        .searchInput(searchInput)
-                        .count(1)
-                        .date(new Date())
-                        .build();
-            }
-
-            searchInputCountRepository.save(searchInputCount); // 저장된 엔티티 수정 후 다시 저장
-
-            searchInputCountRepository.flush(); // 변경 사항을 즉시 데이터베이스에 반영(쓰기 지연 현상 x)
-
-            // Send data to server using restTemplate
-            sendSearchDataToServer(searchInput);
-
-
-
     }
 }
