edf9fa5dc9fb7dea94a5a54022e589ddc03745c4
ujuhye <wngp890@naver.com>
2023-06-08 14:28:48 +0900

    영민,주혜 react 공용

--- frontend/App.js
+++ frontend/App.js
@@ -4,10 +4,8 @@ import './App.css';
 import searchImage from './search.png';                               /*돋보기 이미지*/
 import keyboardImage from './keyboard_icon.png';                      /*키보드 이미지*/
 import lowImage from './▼.png';                                       /*▼ 이미지*/
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NewPage from './NewPage'; // 새 페이지 컴포넌트를 임포트합니다.
-import { useHistory } from 'react-router-dom';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   const [showList, setShowList] = useState(false);                                      /*검색창 체크리스트*/
@@ -16,16 +14,7 @@ function App() {
   const handleCheckboxChange = (event) => {setSelectedOption(event.target.value);};     /* // */
   useEffect(() => {setSelectedOption('식당 검색'); }, []);                               /*앞▼에 기본값 "식당 검색"*/
 
-  const history = useHistory();
-  const handleSearchButtonClick = () => {
-    history.push('/newpage');
-  };
-
   return (
-    <Router>
-      <Route path="/" exact component={App} />
-      <Route path="/newpage" component={NewPage} />
- 
     <main className="container">
       <h1 className="title">Na</h1>
       <h2 className="subtitle1">의 솔직</h2>
@@ -43,7 +32,7 @@ function App() {
       <button className="low-button">                                                           {/*▼ 버튼*/}
         <img src={lowImage} alt="low" className="low-icon" />                                   {/* // */}
       </button>                                                                                 {/* // */}
-      <button className="search-button" onClick={handleSearchButtonClick}>                                                        {/*돋보기 버튼*/}
+      <button className="search-button">                                                        {/*돋보기 버튼*/}
         <img src={searchImage} alt="Search" className="search-icon" />                          {/* // */}
       </button>                                                                                 {/* // */}
       {selectedOption && !showList && (                                                          /*앞▼ 기본값(식당검색) 표시=이 조건이 참일 때에만 선택한 항목이 출력*/
@@ -90,9 +79,7 @@ function App() {
       </ul>
       {showList && (<div className="selected-option">{selectedOption}</div>)}    {/*리스트 선택한 항목을 보여줌 = 선택항목은 selectedOption 상태에 저장, 체크박스를 클릭할 때마다 handleCheckboxChange 함수 호출돼 선택항목을 업데이트 */}
     </main>
-    </Router>
   );
 }
-
 
 export default App;                   /* 컴포넌트를 외부로 내보내기(exports)위해 사용. 다른 파일에서 이 컴포넌트를 불러와 사용 */
