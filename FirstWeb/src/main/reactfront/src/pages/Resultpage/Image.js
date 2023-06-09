c126ba98f1a49a98ec34b632e6dff18053d393fe
E0min <bbok4yo@gmail.com>
2023-06-09 22:02:56 +0900

    cm

--- FirstWeb/src/main/reactfront/src/pages/Resultpage/Image.js
+++ FirstWeb/src/main/reactfront/src/pages/Resultpage/Image.js
@@ -3,6 +3,7 @@ import { Box, CircularProgress } from '@mui/material';
 
 export default function CustomImageList({ data }) {
   const [imgLoading, setImgLoading] = useState(Array(data.imageURL.length).fill(true));
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleImageLoad = (index) => {
     setImgLoading(prevState => {
@@ -12,19 +13,38 @@ export default function CustomImageList({ data }) {
     });
   };
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   if (!data || !data.imageURL) {
     return null;
   }
 
   const imageURL = data.imageURL;
+  const imageHeight = isHovered ? '50vh' : '30vh';
 
   return (
     <div style={{ display: 'flex', overflowX: 'auto' }}>
       {imageURL.map((img, index) => {
         const imageUrl = img.replace(/'/g, ""); // 작은 따옴표를 제거합니다.
         return (
-          <div key={index} style={{ marginRight: '5px' }}>
-            <img src={imageUrl} alt={`Image ${index}`} onLoad={() => handleImageLoad(index)} style={{ width: 'auto', height: '30vh', borderRadius: '5px' }}/>
+          <div
+            key={index}
+            style={{ marginRight: '5px' }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <img
+              src={imageUrl}
+              alt={`Image ${index}`}
+              onLoad={() => handleImageLoad(index)}
+              style={{ width: 'auto', height: imageHeight, borderRadius: '5px' }}
+            />
             {imgLoading[index] && <CircularProgress />}
           </div>
         );
